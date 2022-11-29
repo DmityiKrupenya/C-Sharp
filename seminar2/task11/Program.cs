ebe5a05b53323a1149053fbdfcb35423af8beebe
Dima <zergofoo@gmail.com>
2022-11-29 16:06:41 +0300

    Add task9,task11,task12

---
+++ seminar2/task11/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 11: Напишите программу, которая выводит случайное трёхзначное число и 
+//удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int a = new Random().Next(100, 1000);
+Console.WriteLine(a);
+int num3 = a % 10;
+int num1 = a / 100;
+
+Console.WriteLine(num1);
+Console.WriteLine(num3);
+
+Console.WriteLine($"Искомое число = {num1 * 10 + num3}");
---
+++ seminar2/task9/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 9. Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+//и показывает наибольшую цифру числа.
+//78 -> 8
+//12-> 2
+//85 -> 8
+
+int a = new Random().Next(10, 100);
+Console.WriteLine(a);
+int number1 = a % 10;
+int number2 = a / 10;
+if (number1 > number2)
+{
+    Console.WriteLine(number1);
+}
+else 
+{
+    Console.WriteLine(number2);
+}
+Console.WriteLine($"Первая цифра = {number1}, вторая цифра = {number2}");

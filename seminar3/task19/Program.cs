9b108645c29f983f4aa58fd184de73eb6a767c3a
Dima <zergofoo@gmail.com>
2022-12-03 21:00:14 +0300

    add task19

---
+++ seminar3/task19/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+Console.WriteLine("Введите число для проверки на полиндром");
+string num = Console.ReadLine();
+
+void CheckNum(string num)
+{
+  if (num[0]==num[4] || num[1]==num[3]){
+    Console.WriteLine($"Ваше число: {num} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число: {num} - НЕ палиндром.");
+}
+
+if (num.Length == 5){
+  CheckNum(num);
+}
+else Console.WriteLine($"Введите правильное число");
---
+++ seminar3/task21/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 21
+
+// Напишите программу, которая принимает на вход координаты 
+//двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("enter xA");
+double xA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter yA");
+double yA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter zA");
+double zA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter xB");
+double xB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter yB");
+double yB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter zB");
+double zB = Convert.ToInt32(Console.ReadLine());
+
+double resX = Math.Pow(xB - xA, 2);
+double resY = Math.Pow(yB - yA, 2);
+double resZ = Math.Pow(zB - zA, 2);
+double result = Math.Sqrt(resX+resY+resZ);
+Console.WriteLine($"{result:f4}");
+

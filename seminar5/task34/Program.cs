28e7b4c28304bdf6bc554e05bec6005763f11a48
Dima <zergofoo@gmail.com>
2022-12-12 17:45:19 +0300

    add task34 and task26

---
+++ seminar5/task34/Program.cs
@@ -0,0 +1,34 @@
+﻿// - [ ]  Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine()); 
+int[] numbers = new int[size];  //создание массива
+FillArrayRandomNumbers(numbers); 
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int z = 0; z < numbers.Length; z++) // нахождение четных чисел
+if (numbers[z] % 2 == 0) // четность числа
+count++;
+
+Console.WriteLine($"всего {numbers.Length} чисел, чётные: {count} ");
+
+void FillArrayRandomNumbers(int[] numbers) // метод заполнения массива 
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000); 
+    }
+}
+void PrintArray(int[] numbers) // метод вывода массива в консоль
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}   
---
+++ seminar5/task36/Program.cs
@@ -0,0 +1,36 @@
+﻿// - [ ]  **Задача 36:** Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size]; // создание массива
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i = i + 2)
+{
+    sum = sum + numbers[i];
+}
+Console.WriteLine(sum);
+
+
+void FillArrayRandomNumbers(int[] numbers) // метод заполнения массива 
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(-1000, 1000); 
+    }
+}
+
+void PrintArray(int[] numbers) // метод вывода массива в консоль
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}

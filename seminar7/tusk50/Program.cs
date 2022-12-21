3571121958a1d0a3d754588a8592cffe189af759
Dima <zergofoo@gmail.com>
2022-12-21 11:36:07 +0300

    add tusk 47,50,52

---
+++ seminar7/tusk50/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в 
+//двумерном массиве, и возвращает значение этого элемента или же указание,
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// ​
+// 5 9 2 3
+// ​
+// 8 4 2 4 ​
+// 1 7 -> такого числа в массиве нет
+
+Console.WriteLine("Введите количество строк в массиве");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в массиве");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int[x, y];// обьявим массив
+Random ran = new Random();
+void InitializingMas(int[,] arr)
+{
+    //инициализация данного массива
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            arr[i, j] = ran.Next(1, 35);
+        }
+    }
+}
+void PrintArray(int[,] arr) // метод распечатывания массива
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int y = 0; y < arr.GetLength(1); y++)
+        {
+            Console.Write(arr[i, y] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+InitializingMas(arr);
+PrintArray(arr);
+
+Console.WriteLine("Введите 1 координату в массиве");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 2 координату в массиве");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > x && b > y)
+{
+    Console.WriteLine("такого числа нет");
+}
+else
+{
+    object? c = arr.GetValue(a, b);
+    Console.WriteLine(c); 
+}
+
+
+
+
+
+
---
+++ seminar7/tusk52/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+
+
+Console.WriteLine("Введите количество строк в массиве");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в массиве");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int[x, y];// обьявим массив
+Random ran = new Random();
+void InitializingMas(int[,] arr)
+{
+    //инициализация данного массива
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            arr[i, j] = ran.Next(1, 35);
+        }
+    }
+}
+void PrintArray(int[,] arr) // метод распечатывания массива
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int y = 0; y < arr.GetLength(1); y++)
+        {
+            Console.Write(arr[i, y] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+InitializingMas(arr);
+PrintArray(arr);
+
+void srAr(int[,] arr)
+{
+    for (int i = 0; i < y; i++)
+    {
+        int srAr = 0;
+        for (int j = 0; j < x; j++)
+        {   
+            srAr += arr[j, i];
+        }
+        Console.WriteLine($"Среднее арифмитическое столбцов = {(float)srAr / y}");
+    }
+}
+srAr(arr);
